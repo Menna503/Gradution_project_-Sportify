eb853bed860d4b6399130f805a9061128b780053
mai <maimahmud47@gmail.com>
2025-07-07 04:32:55 +0300

    noting

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/products/cart.service';
 import { AuthService } from '../../services/auth/authservice/auth.service';
@@ -8,85 +8,82 @@ import { FavoritesService } from '../../services/favorites/favorites.service';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule ,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
-  fname:string |null='' ;
-  email:string |null='';
-  show:string='hidden'
-  ishidden:boolean=false;
-   token:string|null=null;
-   data:any;
- showLoginPrompt: boolean = false;
+  fname: string | null = '';
+  email: string | null = '';
+  show: string = 'hidden';
+  ishidden: boolean = false;
+  token: string | null = null;
+  data: any;
+  showLoginPrompt: boolean = false;
 
- cartItemCount: number = 0;
- favoriteItemCount: number = 0;
+  cartItemCount: number = 0;
+  favoriteItemCount: number = 0;
 
-  constructor(private authService:AuthService ,private router:Router,
-    private cartService: CartService,  private favoritesService: FavoritesService
-)
-  {
-    this.token=localStorage.getItem('token');
-     console.log(this.token);
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cartService: CartService,
+    private favoritesService: FavoritesService
+  ) {
+    this.token = localStorage.getItem('token');
+    console.log(this.token);
   }
   ngOnInit() {
-     this.token=localStorage.getItem('token');
-      console.log(this.token);
-       this.fname=localStorage.getItem('Fname');
-       this.email=localStorage.getItem('Email')
+    this.token = localStorage.getItem('token');
+    console.log(this.token);
+    this.fname = localStorage.getItem('Fname');
+    this.email = localStorage.getItem('Email');
 
-       this.cartService.cartCount$.subscribe(count => {
-        console.log('Updated cart count:', count); 
-        this.cartItemCount = count;
-      });
-    this.favoritesService.favoriteCount$.subscribe(count => {
-  this.favoriteItemCount = count;
-});
+    this.cartService.cartCount$.subscribe((count) => {
+      console.log('Updated cart count:', count);
+      this.cartItemCount = count;
+    });
+    this.favoritesService.favoriteCount$.subscribe((count) => {
+      this.favoriteItemCount = count;
+    });
   }
 
   toggel() {
     this.show = this.show === 'hidden' ? 'block' : 'hidden';
   }
-  
-  getProfile(){
- 
-  this.ishidden=!this.ishidden
+
+  getProfile() {
+    this.ishidden = !this.ishidden;
   }
 
-  logout(){
+  logout() {
     this.authService.signout();
     this.router.navigate(['/home'], { replaceUrl: true });
-    this.token='';
-    this.ishidden=false;
-    localStorage.removeItem('cart'); 
+    this.token = '';
+    this.ishidden = false;
+    localStorage.removeItem('cart');
   }
-  
+
   isAdmin(): boolean {
     return localStorage.getItem('role') === 'admin';
   }
 
-navigateToLogin() {
-  this.router.navigate(['/login']);
-}
-checkLoginBeforeNavigate(category: string, event: Event) {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    event.preventDefault();
-    this.showLoginPrompt = true;
-    return;
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+  checkLoginBeforeNavigate(category: string, event: Event) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      event.preventDefault();
+      this.showLoginPrompt = true;
+      return;
+    }
+
+    this.show = 'hidden';
   }
 
-  // ✅ مش لازم نستخدم router.navigate هنا
-  this.show = 'hidden';
-}
-
-
-
-goHome() {
+  goHome() {
     console.log('Navigating to home...');
     this.router.navigate(['/home']);
   }
-
 }
