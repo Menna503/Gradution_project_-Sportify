33b06400a94b757d046201241630779ac2dbdd97
mai <maimahmud47@gmail.com>
2025-07-08 19:15:42 +0300

    fix in imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,40 +5,55 @@ import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { FavComponent } from './pages/fav/fav.component';
+import { PaymentPageComponent } from './pages/payment-page/payment-page.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { SuplementsComponent } from './pages/suplements/suplements.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PartnersComponent } from './components/partners/partners.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConfirmPaymentComponent } from './pages/confirm-payment/confirm-payment.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 @Component({
   selector: 'app-root',
 
-  providers: [
-    { provide: ErrorHandler, useClass: GlobalErrorHandler },
-  ],
+  providers: [{ provide: ErrorHandler, useClass: GlobalErrorHandler }],
 
   imports: [
     RouterOutlet,
     CommonModule,
-    FavComponent,
-    PaymentPageComponent, 
-    CheckOutComponent,
-    HomeComponent,
-    MenComponent,
-    WomenComponent,
-    SuplementsComponent,
-    FooterComponent,
-    PartnersComponent, 
-    HeaderComponent,
-    ConfirmPaymentComponent,
-    PaginationComponent,
-    HttpClientModule,
-    SignupPageComponent,
-    SigninComponent,
-    CartComponent,
-    CartPageComponent,
-    ProductDetailsComponent,
-    ProfileComponent
+    // FavComponent,
+    // PaymentPageComponent,
+    // CheckOutComponent,
+    // HomeComponent,
+    // MenComponent,
+    // WomenComponent,
+    // SuplementsComponent,
+    // FooterComponent,
+    // PartnersComponent,
+    // HeaderComponent,
+    // ConfirmPaymentComponent,
+    // PaginationComponent,
+    // HttpClientModule,
+    // SignupPageComponent,
+    // SigninComponent,
+    // CartComponent,
+    // CartPageComponent,
+    // ProductDetailsComponent,
+    // ProfileComponent,
+    RouterOutlet,
+    CommonModule,
   ],
-
-
-  imports: [RouterOutlet, CommonModule],
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
