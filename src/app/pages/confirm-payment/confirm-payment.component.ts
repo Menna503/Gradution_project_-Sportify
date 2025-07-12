18cd7f0dfc0816dae873751cc0032aca92bd271c
ghada harb <ghadaharp2000@gmail.com>
2025-07-12 10:18:06 +0300

    Refactor: Order confirmation after payment

--- src/app/pages/confirm-payment/confirm-payment.component.ts
+++ src/app/pages/confirm-payment/confirm-payment.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/products/cart.service';
 import { FormsModule } from '@angular/forms';
+import { PaymentService } from '../../services/payment.service';
+import { ToastrService } from 'ngx-toastr';
+import { LoadingComponent } from '../../components/loading/loading.component';
 
 @Component({
   selector: 'app-confirm-payment',
@@ -14,12 +17,57 @@ import { FormsModule } from '@angular/forms';
     RouterModule,
     CommonModule,
     FormsModule,
+    LoadingComponent,
   ],
   templateUrl: './confirm-payment.component.html',
   styleUrl: './confirm-payment.component.css',
 })
-export class ConfirmPaymentComponent {
-  constructor(private router: Router, private cartService: CartService) {}
+export class ConfirmPaymentComponent implements OnInit {
+  loading = true;
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private route: ActivatedRoute,
+    private paymentService: PaymentService,
+    private toastr: ToastrService,
+    private ngZone: NgZone
+  ) {}
+  ngOnInit(): void {
+    const sessionId = this.route.snapshot.queryParamMap.get('session_id');
+    const address = this.getShippingDetailsFromLocalStorage();
+    if (!sessionId) {
+      this.loading = false;
+      this.router.navigate(['/payment']);
+      // this.router.navigate(['/confirmPayment']);
+      return;
+    }
+    if (sessionId && address) {
+      this.paymentService.placeStripeOrder(sessionId, address).subscribe({
+        next: (res) => {
+          this.clearCartData();
+          this.loading = false;
+          this.toastr.success('Order placed successfully!', 'Success');
+          // this.router.navigate(['/confirmPayment']);
+          this.ngZone.run(() => {
+            setTimeout(() => {
+              this.router.navigate(['/confirmPayment']);
+            }, 1000);
+          });
+        },
+        error: () => {
+          this.toastr.error(
+            'Stripe payment was made but order failed.',
+            'Error'
+          );
+          this.loading = false;
+          this.router.navigate(['/decline-order']);
+        },
+      });
+    } else {
+      // this.toastr.error('Missing payment session or address.');
+      // this.router.navigate(['/decline-order']);
+    }
+  }
 
   getShippingDetailsFromLocalStorage(): {
     address: string;
@@ -34,7 +82,13 @@ export class ConfirmPaymentComponent {
   successPayment() {
     this.router.navigate(['/home']);
   }
-    TrackMyOrder() {
+  TrackMyOrder() {
     this.router.navigate(['/profile']);
+  }
+  clearCartData(): void {
+    localStorage.removeItem('totalPrice');
+    localStorage.removeItem('cart');
+    localStorage.removeItem('shippingAddress');
+    this.cartService.clearCart();
   }
 }
